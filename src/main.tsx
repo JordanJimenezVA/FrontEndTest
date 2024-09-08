2d4117e7a57ad813d59855a833a136d1f0bcb268
JordanJimenezVA <jordan.jimenez.valenzuela@gmail.com>
2024-09-08 04:33:16 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App.tsx';
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <App />
-// );
-
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
